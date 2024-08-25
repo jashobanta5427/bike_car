65259829c94c0d0859878991f5e55ea653b52870
Jashobanta Behera <jashobanta5427@gmail.com>
2024-08-25 16:22:27 +0530

    First commit

---
+++ src/main/java/com/sevice/bike_car/BikeCarApplication.java
@@ -0,0 +1,13 @@
+package com.sevice.bike_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevice/bike_car/BikeCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevice.bike_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
